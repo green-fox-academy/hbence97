fc74ba3ac0a44c6eeee84eeba295a89b21d7b6f1
hbence97 <hegyesbence@gmail.com>
2019-01-17 18:01:55 +0100

    3-4

---
+++ week-01/day-04/feladat 3 Append-a-func/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+// - Create a string variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(typo)`
+
+void appendAFunc(std::string TYPO) {
+    std::cout << TYPO << "a" << std::endl;
+};
+
+int main(int argc, char* args[]) {
+
+    std::string typo = "Chinchill";
+    appendAFunc(typo);
+
+    return 0;
+}
