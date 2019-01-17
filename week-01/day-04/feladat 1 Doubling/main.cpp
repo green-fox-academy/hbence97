44670401b6dd6a4c8efca8e782172476eb0123f9
hbence97 <hegyesbence@gmail.com>
2019-01-17 14:38:21 +0100

    doubling

---
+++ week-01/day-04/feladat 1 Doubling/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+void doubling(int&);
+
+
+int main(int argc, char* args[]) {
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+    // - Print the result of `doubling(baseNum)`
+
+    int baseNum = 123;
+    doubling(baseNum);
+    std::cout << baseNum <<std::endl;
+    return 0;
+}
+void doubling(int& baseNum) {
+    baseNum *= 2;
+
+}
