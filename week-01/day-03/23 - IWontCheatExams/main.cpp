15aebe83c881998554adcec77fb3c3bb65a8a838
hbence97 <hegyesbence@gmail.com>
2019-01-18 16:12:40 +0100

    day 2

---
+++ week-01/day-03/23 - IWontCheatExams/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    int main(int argc, char* args[]) {
+
+    int a = 0;
+    while (a < 100) {
+        std::cout << "I won't cheat on exams" << std::endl;
+        a += 1;
+    }
+
+    for (int i = 0; i < 100 ; i++) {
+
+    }
+    return 0;
+}
