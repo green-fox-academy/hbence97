4a0fb4784ebc06c2a6f1e6b50139113d508ed2ec
hbence97 <hegyesbence@gmail.com>
2019-01-20 13:54:18 +0100

    practice day

---
+++ week-01/day-05/07 - Third/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+    int main(int argc, char* args[]) {
+    int q [4] = {4, 5, 6, 7};
+    std::cout << q [2] << std::endl;
+
+    return 0;
+}
