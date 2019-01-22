761da19f42a8de830ac0764bf7ae41a8a2b7ccd5
hbence97 <hegyesbence@gmail.com>
2019-01-22 15:10:30 +0100

    day 2

---
+++ week-02/day-02/05 - PrintAdresses/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+    // Create a program which accepts five integers from the console (given by the user)
+    // and store them in an array
+    // print out the memory addresses of the elements in the array
+    int main()
+{
+    int numbers[5];
+
+    std::cout << "Give me 5 numbers :" << std::endl;
+
+
+    for (int i = 0 ; i < 5 ; i++){
+        std::cin >> numbers[i];
+
+    }
+    for (int i = 0 ; i < 5 ; i++) {
+        std::cout << &numbers[i];
+
+    }
+    return 0;
+}
