286a6e38d3433093876dce9ebc12b901c262658c
hbence97 <hegyesbence@gmail.com>
2019-02-01 17:12:17 +0100

    day 3

---
+++ week-03/day-03/08 - DiceSet/dice_set.h
@@ -0,0 +1,26 @@
+//
+// Created by hegye on 2019. 02. 01..
+//
+
+#ifndef DICE_SET_DICE_SET_H
+#define DICE_SET_DICE_SET_H
+
+#include <iostream>
+#include <vector>
+
+class DiceSet
+{
+public:
+
+    DiceSet();
+
+    void roll();
+    void roll(int i);
+    std::vector<int> getCurrent() const;
+    int getCurrent(int i) const;
+private:
+    std::vector<int> dices;
+};
+
+
+#endif //DICE_SET_DICE_SET_H
