11fd0ada872e4570e6cc5b6ea42fd93804d13ce7
hbence97 <hegyesbence@gmail.com>
2019-02-01 16:31:01 +0100

    day 3

--- week-03/day-03/07 - FleetOfThings/main.cpp
+++ week-03/day-03/07 - FleetOfThings/main.cpp
@@ -11,6 +11,18 @@ int main(int argc, char* args[])
     // 3. [x] Stand up
     // 4. [x] Eat lunch
 
+    Thing thing1("Get Milk");
+    Thing thing2("Remove the obstacles");
+    Thing thing3("Stand Up");
+    thing3.complete();
+    Thing thing4("Eat lunch");
+    thing4.complete();
+
+    fleet.add(thing1);
+    fleet.add(thing2);
+    fleet.add(thing3);
+    fleet.add(thing4);
+
     std::cout << fleet.toString() << std::endl;
     return 0;
 }
