a1add32b6e36316fad89cf83bbe78401caba26c8
hbence97 <hegyesbence@gmail.com>
2019-02-06 15:03:21 +0100

    added Sponsor

---
+++ week-04/day-02/01 - GreenFox/sponsor.h
@@ -0,0 +1,14 @@
+//
+// Created by hegye on 2019. 02. 06..
+//
+
+#ifndef INC_01_GREENFOX_SPONSOR_H
+#define INC_01_GREENFOX_SPONSOR_H
+
+
+class sponsor {
+
+};
+
+
+#endif //INC_01_GREENFOX_SPONSOR_H
