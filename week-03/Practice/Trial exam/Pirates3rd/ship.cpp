04861dac32c2d02ee73ae7bd33c57cdd0e99b21c
hbence97 <hegyesbence@gmail.com>
2019-02-11 22:56:42 +0100

    almost done, have to sleep tho

---
+++ week-03/Practice/Trial exam/Pirates3rd/ship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hegye on 2019. 02. 11..
+//
+
+#include "ship.h"
---
+++ week-03/Practice/Trial exam/Pirates3rd/ship.h
@@ -0,0 +1,16 @@
+//
+// Created by hegye on 2019. 02. 11..
+//
+
+#ifndef PIRATES3RD_SHIP_H
+#define PIRATES3RD_SHIP_H
+
+
+
+class ship {
+
+};
+
+
+
+#endif //PIRATES3RD_SHIP_H
