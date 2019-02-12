f474718e74ee67ea5cf358176ca1993f80da4169
hbence97 <hegyesbence@gmail.com>
2019-02-12 17:39:44 +0100

    carrier added, and aircrafts not working hehe

---
+++ week-03/Practice/Inheritance/AircraftCarrier/carrier.h
@@ -0,0 +1,16 @@
+//
+// Created by hegye on 2019. 02. 12..
+//
+
+#ifndef AIRCRAFTCARRIER_CARRIER_H
+#define AIRCRAFTCARRIER_CARRIER_H
+
+
+
+class carrier {
+
+};
+
+
+
+#endif //AIRCRAFTCARRIER_CARRIER_H
