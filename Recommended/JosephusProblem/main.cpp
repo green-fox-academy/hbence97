58b0f5449101d371ff1a754abc4497fa3e6257c4
hbence97 <hegyesbence@gmail.com>
2019-02-20 20:41:14 +0100

    Guess my number replaced, Josephus problem created

---
+++ Recommended/JosephusProblem/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int numberOfPeople;
+
+    std::cout << "How many people do you want to be participated in your game? " << std::endl;
+    std::cin >> numberOfPeople;
+
+    return 0;
+}
