1e40045d5f8592b355bf9e26143402558d37ea15
hbence97 <hegyesbence@gmail.com>
2019-02-26 18:30:10 +0100

    Exercise 2

--- Retake/Exercise2/main.cpp
+++ Retake/Exercise2/main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+
 std::map<std::string, int> chipUsageData(std::string fileName);
 
 int main() {
@@ -18,27 +19,36 @@ int main() {
     return 0;
 }
 
-std::map<std::string, int> chipUsageData(std::string fileName)
-{
+std::map<std::string, int> chipUsageData(std::string fileName) {
     std::ifstream logFile;
     logFile.open(fileName);
 
-    if(!logFile.is_open())
-        throw std::string ("Failed to open file");
+    if (!logFile.is_open())
+        throw std::string("Failed to open file");
 
-    std::map<std::string,int> result;
+    std::map<std::string, int> result;
+
     std::string line;
 
-    std::string cardNumber;
-    //int cardUsage = 0; ( különbség?)
-
-
-    while(std::getline(logFile,line)){
+    while (std::getline(logFile, line)) {
         std::stringstream lineStream(line);
+        int counter = 0;
+        std::string entranceName;
+        std::string cardNumber;
 
-        std::getline(lineStream,cardNumber,',');
-            result[cardNumber]++;//= cardUsage;
-
+        std::string data;
+        while(std::getline(lineStream, data, ',')) {
+            if (counter == 5) {
+                entranceName = data;
+            }
+            if (counter == 12){
+                cardNumber = data;
+            }
+            counter++;
+        }
+        if(entranceName == "A66 - 04 FÕBEJÁRAT (F-1) Door #1" ) {
+            result[cardNumber]++;
+        }
     }
     logFile.close();
 
