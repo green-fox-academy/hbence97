4c0928016d66a382aec3af3d9e4727f83c311cd4
hbence97 <hegyesbence@gmail.com>
2019-02-26 22:46:40 +0100

    Reretake practice exercise 1

--- Reretake/exercise1/main.cpp
+++ Reretake/exercise1/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <vector>
 
+int numberPairs(std::vector<char> arr1, std::vector <char> arr2);
+void runFirstTest()
+{
+    std::vector<char> test1 = {'a', 'b', 'c'};
+    std::vector<char> test2 = {'c', 'b', 'd', 'e'};
+
+    int result = numberPairs(test1, test2);
+
+    if (result == 2)
+    {
+        std::cout << "First test passed" << std::endl;
+    } else {
+        std::cout << "First test failed" << std::endl;
+    }
+}
 int main() {
-    
+    runFirstTest();
     return 0;
+}
+
+int numberPairs(std::vector<char> arr1, std::vector <char> arr2)
+{
+    int result = 0;
+    arr1 = {'a', 'b', 'c'};
+    arr2 = {'c', 'b', 'd', 'e'};
+    for (int i = 0; i < arr1.size(); ++i) {
+        for (int j = 0; j < arr2.size(); ++j) {
+            if (arr1[i] == arr2[j])
+                result++;
+        }
+    }
+    return result;
 }
