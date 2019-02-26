58faf5a739856b4cb9095d02d68688993f6e1d61
hbence97 <hegyesbence@gmail.com>
2019-02-26 09:54:57 +0100

    exercise 2 continued

--- Retake/Exercise2/main.cpp
+++ Retake/Exercise2/main.cpp
@@ -2,26 +2,45 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+std::map<std::string, int> chipUsageData(std::string fileName);
 
 int main() {
+    try {
+        std::map<std::string, int> chipUsage = chipUsageData("logs.csv");
 
+        //Print the result
+        for (std::map<std::string, int>::iterator it = chipUsage.begin(); it != chipUsage.end(); ++it) {
+            std::cout << it->first << " - " << it->second << std::endl;
+        }
+    } catch (std::string e) {
+        std::cout << e << std::endl;
+    }
     return 0;
 }
 
 std::map<std::string, int> chipUsageData(std::string fileName)
 {
-    std::map<std::string,int> result;
-
     std::ifstream logFile;
     logFile.open(fileName);
 
     if(!logFile.is_open())
         throw std::string ("Failed to open file");
+
+    std::map<std::string,int> result;
     std::string line;
+
+    std::string cardNumber;
+    //int cardUsage = 0; ( különbség?)
+
+
     while(std::getline(logFile,line)){
-        std::stringstream stringstream;
-        std::string ChipUsageData;
-        std::getline(line,ChipUsageData,', ');
+        std::stringstream lineStream(line);
+
+        std::getline(lineStream,cardNumber,',');
+            result[cardNumber]++;//= cardUsage;
 
     }
+    logFile.close();
+
+    return result;
 }
