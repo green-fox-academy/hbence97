ca6dc2747faa5aaca69fd8dfbdc655906bdefc4c
hbence97 <hegyesbence@gmail.com>
2019-02-27 17:59:59 +0100

    C beggining, day 1

---
+++ week-06/day-01/01 - HelloMe/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Bence!\n");
+    return 0;
+}
---
+++ week-06/day-01/02 - HelloOthers/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, Lilla!\n");
+    printf("Hello, Tomi!\n");
+    printf("Hello, Zoltán\n");
+    return 0;
+}
