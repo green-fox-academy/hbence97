8356afcf43d8177a2f672361903e87f3557fe28a
hbence97 <hegyesbence@gmail.com>
2019-03-02 17:26:20 +0100

    Sandwiches

--- week-06/day-02/02 - Sandwich/main.c
+++ week-06/day-02/02 - Sandwich/main.c
@@ -12,8 +12,37 @@ The parameters should be:
  - and an integer that represents how many sandwiches you want to order
 */
 
+typedef struct
+{
+    char* _name;
+    float _price;
+    float _weight;
+    int _order;
+} Sandwich_t;
+
+float Price(Sandwich_t sandwich, int _order);
+
 int main()
 {
+    Sandwich_t sandwich1;
+    sandwich1._name = "Cheeseburger";
+    sandwich1._price = 3;
+    sandwich1._weight = 1;
+    sandwich1._order = 12;
+
+    Sandwich_t sandwich2;
+    sandwich2._name = "Hamburger";
+    sandwich2._price = 2.5;
+    sandwich2._weight = 0.9;
+    sandwich2._order = 5;
+
+    printf("The price of your sandwiches will be %.1f dollars\n", Price(sandwich1, sandwich1._order));
+    printf("The price of your sandwiches will be %.1f dollars\n", Price(sandwich2, sandwich2._order));
 
     return 0;
+}
+
+float Price(Sandwich_t sandwich, int _order)
+{
+    return (sandwich._price * _order);
 }
