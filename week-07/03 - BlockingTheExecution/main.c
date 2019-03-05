4ab5488d310508b8a3195556a282ea65169c8362
hbence97 <hegyesbence@gmail.com>
2019-03-05 15:22:39 +0100

    Blocking the execution

---
+++ week-07/03 - BlockingTheExecution/main.c
@@ -0,0 +1,22 @@
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+int main(void)
+{
+    HAL_Init();
+
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+
+    BSP_LED_Init(LED1);
+
+    while (1) {
+    	if(BSP_PB_GetState(BUTTON_KEY)){
+    		while(1){
+			BSP_LED_On(LED1);
+			HAL_Delay(250);
+			BSP_LED_Off(LED1);
+			HAL_Delay(250);
+    	}
+    }
+}
+}
