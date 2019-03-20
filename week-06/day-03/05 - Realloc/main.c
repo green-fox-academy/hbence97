829224e292997bc1e5cea2cac2a8e5dfee93757e
hbence97 <hegyesbence@gmail.com>
2019-03-20 11:58:24 +0100

    Realloc

---
+++ week-06/day-03/05 - Realloc/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prompt the user to enter a number. -> This number will be X.
+// Allocate memory for X numbers.
+// Prompt the user to enter X numbers.
+// Allocate memory for Y numbers, which is the sum of the X numbers.
+// Fill that array with from 1 to Y, then print them.
+
+int main()
+{
+    int* pointer = NULL;
+    printf("How many numbers do you want to enter:\n");
+    int x = 0;
+    scanf("%d", &x);
+    pointer = (int *) malloc(x * sizeof(int));
+    for (int i = 0; i < x; ++i) {
+        printf("Now give me the numbers! %d \n", i + 1);
+            scanf("%d", &pointer[i]); //!
+    }
+    int y = 0;
+    for (int i = 0; i < x; ++i) { //miért i?
+        y += pointer[i];
+    }
+
+    realloc(pointer, y * sizeof(pointer));
+
+    for (int j = 0; j < y; ++j) {
+        
+    }
+    
+    return 0;
+}
