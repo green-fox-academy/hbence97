dd19cef23279670d424823fd37f1647e4c9ed459
hbence97 <hegyesbence@gmail.com>
2019-04-10 14:24:30 +0200

    day 1, first 3 tasks

---
+++ 00110010 01101110 01100100/Basics_Of_C/02 - HelloOthers/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    printf("Hello, Tomi!\n");
+    printf("Hello, Zoltan!\n");
+    printf("Hello, Tamas!\n");
+
+    return 0;
+}
