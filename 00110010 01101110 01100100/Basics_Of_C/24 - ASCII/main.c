47d35692401bb33b6489597cb859f327c7d05227
hbence97 <hegyesbence@gmail.com>
2019-04-12 19:36:41 +0200

    Exercise 24

---
+++ 00110010 01101110 01100100/Basics_Of_C/24 - ASCII/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// print out the characters that corresponds to these ascii values
+
+int main()
+{
+    int array[] = {103, 114, 101, 101, 110, 32, 102, 111, 120};
+
+    char string[(sizeof(array) / sizeof(int))];
+    for (int i = 0; i < sizeof(string); ++i) {
+        string[i] = array[i];
+    }
+    printf("%s", string);
+
+    return 0;
+}
