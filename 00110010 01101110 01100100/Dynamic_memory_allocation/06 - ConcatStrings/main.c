b6b98be1e31a979e9d14c0cc3c29d192ff6a2db8
hbence97 <hegyesbence@gmail.com>
2019-04-26 19:34:48 +0200

    Exercise 6

---
+++ 00110010 01101110 01100100/Dynamic_memory_allocation/06 - ConcatStrings/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// write a function which takes 2 strings as parameter, concatenates them together and returns it back.
+// Test it for long and empty strings as well.
+// Try to use the least amount of memory that's possible.
+
+char* concatenates (const char* string1, const char* string2)
+{
+    char* strings = malloc(strlen(string1) + strlen(string2) + 1);
+    strcpy(strings, string1);
+    strcat(strings, string2);
+    return strings;
+}
+
+
+int main()
+{
+    char* s1 = "Thank";
+    char* s2 = " you";
+    char* s = concatenates(s1,s2);
+    printf("%s",concatenates(s1,s2));
+
+    free(s);
+    return 0;
+}
