8cb9f603080f9578da7dbd90c1d1ce04063139f4
hbence97 <hegyesbence@gmail.com>
2019-05-06 19:29:13 +0200

    Scale recipe function not working

--- 00110010 01101110 01100100/Orientation_exam_practice/CookbookTransformation/main.c
+++ 00110010 01101110 01100100/Orientation_exam_practice/CookbookTransformation/main.c
@@ -15,11 +15,14 @@ typedef struct {
 }recipe_t;
 
 recipe_t read_recipe(const char *file_name);
-void scale_recipe(recipe_t recipe, unsigned char number_of_people);
+void scale_recipe(ingredient_t ingredient, unsigned char number_of_people);
 
 int main() {
+    ingredient_t ingredient1;
+    ingredient1.amount_needed = 3;
 
     read_recipe("vegan_curry.txt");
+    scale_recipe(ingredient1,4);
 
     return 0;
 }
@@ -37,9 +40,32 @@ recipe_t read_recipe(const char *file_name)
         fgets(buffer, 150, fp);
         puts(buffer);
     }
+    fclose(fp);
 }
 
-void scale_recipe(recipe_t recipe, unsigned char number_of_people)
+void scale_recipe(ingredient_t ingredient, unsigned char number_of_people)
 {
-    
+    FILE *fp;
+    fp = fopen("vegan_curry_for_x.txt", "w");
+    if (fp == NULL){
+        printf("Failed to open the file.\n");
+    }
+    //number_of_people = (unsigned char)malloc(number_of_people * sizeof(unsigned char));
+    printf("How many do you want to order?\n");
+    scanf("%s",&number_of_people);
+    char buffer[50];
+    while (!feof(fp)){
+        fgets(buffer,50,fp);
+
+        char* ingredient_name = strtok(buffer," ");
+        char* measuring_unit = strtok(NULL," ");
+        char* amount = strtok(NULL,"\n");
+
+        strcpy(ingredient.name_of_ingredient,ingredient_name);
+        strcpy(ingredient.name_of_measuring_unit,measuring_unit);
+        strcpy(ingredient.amount_needed,atoi(amount) * number_of_people);
+
+        fprintf(fp, "%s %s",ingredient_name,measuring_unit);
+    }
+    fclose(fp);
 }
