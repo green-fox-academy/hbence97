45a0b2814bd016dd8b6bb5fb6afd7c28f34328de
hbence97 <hegyesbence@gmail.com>
2019-06-16 20:20:19 +0200

    Exercise 27 - 28

---
+++ 00110010 01101110 01100100/C++_practices_before_project/Expressions_and_control_flow/28/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // triangle like this:
+    //
+    // *
+    // **
+    // ***
+    // ****
+    //
+    // The triangle should have as many lines as the number was
+    int num;
+    std::cout << "Tell me a number." << std::endl;
+    std::cin >> num;
+    for (int i = 1; i <= num; ++i) {
+        for (int j = 1; j <= i ; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
