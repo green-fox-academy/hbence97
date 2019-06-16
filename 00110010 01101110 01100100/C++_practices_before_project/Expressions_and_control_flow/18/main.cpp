eaa5b047b418ba18858531468f40df34735f8972
hbence97 <hegyesbence@gmail.com>
2019-06-16 15:42:04 +0200

    Exercise 18 - 19

--- 00110010 01101110 01100100/C++_practices_before_project/Expressions_and_control_flow/18/main.cpp
+++ 00110010 01101110 01100100/C++_practices_before_project/Expressions_and_control_flow/18/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* args[]) {
+
+    // Write a program that reads a number from the standard input,
+    // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+    int a;
+    std::cout << "Tell me a number and I'll tell you if it's even or odd." << std::endl;
+    std::cin >> a;
+
+    if(a % 2 == 0) {
+        std::cout << "Your number is even." << std::endl;
+    } else {
+        std::cout << "Your number is odd." << std::endl;
+    }
+
     return 0;
 }
