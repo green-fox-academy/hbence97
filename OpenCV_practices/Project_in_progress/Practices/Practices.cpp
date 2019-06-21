d9f1c68108f49f3d871d9e7a4f16dd8654974a95
hbence97 <hegyesbence@gmail.com>
2019-06-21 15:27:24 +0200

    Javitasok Matyi segitsegevel

--- OpenCV_practices/Project_in_progress/Practices/Practices.cpp
+++ OpenCV_practices/Project_in_progress/Practices/Practices.cpp
@@ -5,19 +5,19 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-// Creating the image
-cv::Mat image(800, 800, CV_8UC1, cv::Scalar::all(255));
 // The mouse callback
 void mouseCallback(int event, int x, int y, int flags, void* userdata);
 
 int main(int argc, char** argv)
 {
+	// Creating the image
+	cv::Mat image(800, 800, CV_8UC1, cv::Scalar::all(255));
 	// Creating the basic line
 	cv::line(image, cv::Point(400, 400), cv::Point(800, 400), cv::Scalar(0), 2, 8, 0);
 	// Showing the image
 	imshow("Ablak", image);
 	// Calling the function
-	cv::setMouseCallback("Ablak", mouseCallback,&image);
+	cv::setMouseCallback("Ablak", mouseCallback, &image);
 	// Wait for a keystroke in the window
 	cv::waitKey(0);
 	return 0;
@@ -25,6 +25,8 @@ int main(int argc, char** argv)
 
 void mouseCallback(int event, int x, int y, int flags, void* userdata)
 {
+	cv::Mat &image = *(cv::Mat*)userdata;
+
 	// If the left button on the mouse is down
 	if (flags & cv::MouseEventTypes::EVENT_LBUTTONDOWN)
 	{
