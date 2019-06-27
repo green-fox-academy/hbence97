4503e10a748b8f13e8913c9129eed46626f6615f
hbence97 <hegyesbence@gmail.com>
2019-06-27 15:07:43 +0200

    Git ignore added

--- 00110010 01101110 01100100/Google_test/01 - Apples/Test1/apple.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "apple.h"
-
-std::string apple::getApple()
-{
-	return "apple";
-}
--- 00110010 01101110 01100100/Google_test/01 - Apples/Test1/apple.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-class apple
-{
-public:
-	std::string getApple();
-};
-
