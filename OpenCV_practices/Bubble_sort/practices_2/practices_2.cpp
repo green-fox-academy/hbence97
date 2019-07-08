7dfdde2340c6c208fa5fd4454887836a7a3f34b4
hbence97 <hegyesbence@gmail.com>
2019-07-08 14:16:03 +0200

    Optimized version of Bubble sort

--- OpenCV_practices/Bubble_sort/practices_2/practices_2.cpp
+++ OpenCV_practices/Bubble_sort/practices_2/practices_2.cpp
@@ -24,13 +24,17 @@ void swap(int *firstElement, int *secondElement) {
 }
 
 void bubbleSort(int array[], int n) {
-	for (int i = 0; i < n-1; i++)
-	{
-		for (int j = 0; j < n-i-1; j++)
-		{
+	bool swapped;
+	for (int i = 0; i < n-1; i++) {
+		swapped = false;
+		for (int j = 0; j < n-i-1; j++) {
 			if (array[j] > array[j + 1]) {
 				swap(&array[j], &array[j + 1]);
+				swapped = true;
 			}
+		}
+		if (swapped == false) {
+			break;
 		}
 	}
 }
