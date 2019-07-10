81e0a2b377075395834918474e41a03b5fe0f171
hbence97 <hegyesbence@gmail.com>
2019-07-10 13:59:05 +0200

    Now the bubbleSort function returns a vector

--- OpenCV_practices/Bubble_sort/practices_2/practices_2.cpp
+++ OpenCV_practices/Bubble_sort/practices_2/practices_2.cpp
@@ -2,40 +2,50 @@
 #include <iostream>
 #include <vector>
 
-void bubbleSort(std::vector<int> &array);
-void printArray(std::vector<int> &arr);
-void printArrayBackwards(std::vector<int> &arr);
+std::vector<int> bubbleSort(std::vector<int> &array);
+
+// WHEN THE bubbleSort FUNCTION WAS VOID, WE NEEDED EVERYTHING WHAT IS COMMENTED OUT
+
+//void printArray(std::vector<int> &arr);
+//void printArrayBackwards(std::vector<int> &arr);
 
 int main(int argc, char ** argv)
 {
-	std::vector<int> array = {64, 32, 37, 4, 56, 76, 54, 21, 81};
-	bubbleSort(array);
+	std::vector<int> vector = {64, 32, 37, 4, 56, 76, 54, 21, 81};
+	bubbleSort(vector);
 	std::cout << "Sorted array: " << std::endl;
-	printArray(array);
-	printArrayBackwards(array);
+    for (int i = 0; i < vector.size(); i++)
+    {
+        std::cout << vector[i] << " ";
+    }
+	//printArray(array);
+	//printArrayBackwards(array);
 	
 	return 0;
 }
 
-void bubbleSort(std::vector<int> &array) {
+std::vector<int> bubbleSort(std::vector<int> &originalVector) {
 	bool swapped;
 	int temporary;
-	for (int i = 1; i < array.size(); i++) {
+    std::vector<int> resultVector;
+	for (int i = 1; i < originalVector.size(); i++) {
 		swapped = false;
-		for (int j = 0; j < array.size() - i; j++) {
-			if (array[j] > array[j + 1]) {
-				temporary = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temporary;
+		for (int j = 0; j < originalVector.size() - i; j++) {
+			if (originalVector[j] > originalVector[j + 1]) {
+				temporary = originalVector[j];
+				originalVector[j] = originalVector[j + 1];
+				originalVector[j + 1] = temporary;
 				swapped = true;
 			}
 		}
+        resultVector = originalVector;
 		if (swapped == false) {
 			break;
 		}
 	}
+    return resultVector;
 }
- 
+/*
 void printArray(std::vector<int> &arr) {
 	for (int i =  0; i < arr.size(); i++) {
 		std::cout << arr[i] << " ";
@@ -49,4 +59,4 @@ void printArrayBackwards(std::vector<int> &arr) {
 		std::cout << *i << " ";
 	}
 	std::cout << "\n";
-}
+}*/
